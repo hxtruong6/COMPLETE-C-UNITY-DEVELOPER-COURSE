1187e216bd95a80178ea46178fe8424d32e541b6
HXTRuong <hoangxuantruong98@gmail.com>
2018-04-09 14:10:46 +0700

    Add script textController

--- Text101/Assets/Scripts/TextController.cs
+++ Text101/Assets/Scripts/TextController.cs
@@ -1,21 +1,176 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextController : MonoBehaviour {
+public class TextController : MonoBehaviour
+{
     public Text p_text;
-	// Use this for initialization
-	void Start () {
-	    p_text.text = "Hello Truong ^^!";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Space)) {
-	        p_text.text = "In my last post I talked about"
-	                        + " why it is important to know whether words in English"
-	                        + " are countable or uncountable. However, I didn’t mention the fact that many words can be both countable and uncountable. This post discusses some of the reasons for this.Food and drink is an area where words are commonly countable or uncountable. Often, the substance is uncountable, but a single portion (especially in the context of ordering something in a café) is countable";
-	    }
-	}
+
+    private enum States
+    {
+        cell,
+        mirror,
+        sheets_0,
+        lock_0,
+        cell_mirror,
+        sheets_1,
+        lock_1,
+        freedom
+    };
+
+    private States myState;
+    // Use this for initialization
+    void Start()
+    {
+        myState = States.cell;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(myState);
+        if (myState == States.cell)
+        {
+            state_cell();
+        }
+        else if (myState == States.sheets_0)
+        {
+            state_sheets_0();
+        }
+        else if (myState == States.lock_0)
+        {
+            state_lock_0();
+        }
+        else if (myState == States.mirror)
+        {
+            state_mirror();
+        }
+        else if (myState == States.cell_mirror)
+        {
+            state_cell_mirror();
+        }
+        else if (myState == States.lock_1)
+        {
+            state_lock_1();
+        }
+        else if (myState == States.sheets_1)
+        {
+            state_sheets_1();
+        }
+        else if (myState == States.freedom)
+        {
+            state_freedom();
+        }
+    }
+
+    void state_freedom()
+    {
+        p_text.text = "You are FREE!\n\n" +
+                        "Press P to Play again";
+        if (Input.GetKeyDown(KeyCode.P)) { myState = States.cell; }
+
+    }
+
+    void state_cell()
+    {
+        p_text.text = "You are in a prison cell, and you want to escape. There are " +
+                      "some dirty sheets on the bed, a mirror on the wall, and the door " +
+                      "is locked from the outside.\n\n" +
+                      "Press S to view Sheets, M to view Mirror and L to view Lock";
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myState = States.sheets_0;
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            myState = States.mirror;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            myState = States.lock_0;
+        }
+    }
+
+    void state_sheets_0()
+    {
+        p_text.text = "You can't believe you sleep in these things. Surely it's " +
+                    "time somebody changed them. The pleasures of prison life " +
+                    "I guess!\n\n" +
+                    "Press R to Return to roaming your cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell;
+        }
+    }
+
+    void state_lock_0()
+    {
+        p_text.text = "This is one of those button locks. You have no idea what the " +
+                      "combination is. You wish you could somehow see where the dirty " +
+                      "fingerprints were, maybe that would help.\n\n" +
+                      "Press R to Return to roaming your cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell;
+        }
+    }
+
+    void state_mirror()
+    {
+        p_text.text = "The dirty old mirror on the wall seems loose.\n\n" +
+                      "Press T to Take the mirror, or R to Return to cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell;
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            myState = States.cell_mirror;
+        }
+    }
+
+    void state_sheets_1()
+    {
+        p_text.text = "Holding a mirror in your hand doesn't make the sheets look " +
+                      "any better.\n\n" +
+                      "Press R to Return to roaming your cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell_mirror;
+        }
+    }
+
+    void state_cell_mirror()
+    {
+        p_text.text = "You are still in your cell, and you STILL want to escape! There are " +
+                    "some dirty sheets on the bed, a mark where the mirror was, " +
+                    "and that pesky door is still there, and firmly locked!\n\n" +
+                    "Press S to view Sheets, or L to view Lock";
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myState = States.sheets_1;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            myState = States.lock_1;
+        }
+    }
+
+    void state_lock_1()
+    {
+        p_text.text = "You carefully put the mirror through the bars, and turn it round " +
+                      "so you can see the lock. You can just make out fingerprints around " +
+                      "the buttons. You press the dirty buttons, and hear a click.\n\n" +
+                      "Press O to Open, or R to Return to your cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myState = States.cell_mirror;
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            myState = States.freedom;
+        }
+    }
 }
